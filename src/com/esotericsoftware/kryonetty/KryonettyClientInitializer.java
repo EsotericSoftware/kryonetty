2240f5778219e342e069e950c8476a9f0459cda8
jko <kobohd@gmail.com>
2020-08-07 08:46:36 +0200

    remove shareable codecs
    Signed-off-by: jko <kobohd@gmail.com>

--- src/com/esotericsoftware/kryonetty/KryonettyClientInitializer.java
+++ src/com/esotericsoftware/kryonetty/KryonettyClientInitializer.java
@@ -1,7 +1,5 @@
 package com.esotericsoftware.kryonetty;
 
-import com.esotericsoftware.kryo.Kryo;
-
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -9,20 +7,13 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 public class KryonettyClientInitializer extends ChannelInitializer<SocketChannel> {
-	
-	final Endpoint endpoint;
-	
-	private static Kryo CLIENT_KRYO;
-   private static KryonettyClientHandler CLIENT_HANDLER;
-   private static KryonettyDecoder DECODER;
-   private static KryonettyEncoder ENCODER;
-   
+
+    private final Endpoint endpoint;
+    private final KryonettyClientHandler clientHandler;
+
    public KryonettyClientInitializer(Endpoint endpoint) {
    	this.endpoint = endpoint;
-   	CLIENT_KRYO = endpoint.getKryo();
-   	CLIENT_HANDLER = new KryonettyClientHandler(endpoint);
-   	DECODER = new KryonettyDecoder(CLIENT_KRYO);
-   	ENCODER = new KryonettyEncoder(CLIENT_KRYO, 4 * 1024, 16 * 1024);
+   	clientHandler = new KryonettyClientHandler(endpoint);
    }
 	
    @Override
@@ -31,10 +22,10 @@ public class KryonettyClientInitializer extends ChannelInitializer<SocketChannel
        
        //Add the logging, decoder, and encoder first
        p.addLast(new LoggingHandler(LogLevel.INFO));
-       p.addLast(DECODER);
-       p.addLast(ENCODER);
+       p.addLast(new KryonettyDecoder(endpoint));
+       p.addLast(new KryonettyEncoder(endpoint));
 
        // and then business logic.
-       p.addLast(CLIENT_HANDLER);
+       p.addLast(clientHandler);
    }
 }
--- src/com/esotericsoftware/kryonetty/KryonettyServerInitializer.java
+++ src/com/esotericsoftware/kryonetty/KryonettyServerInitializer.java
@@ -1,7 +1,5 @@
 package com.esotericsoftware.kryonetty;
 
-import com.esotericsoftware.kryo.Kryo;
-
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -9,33 +7,25 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 public class KryonettyServerInitializer extends ChannelInitializer<SocketChannel> {
-	
-	final Endpoint endpoint;
-	
-	private static Kryo SERVER_KRYO;
-   private static KryonettyServerHandler SERVER_HANDLER;
-   private static KryonettyDecoder DECODER;
-   private static KryonettyEncoder ENCODER;
-	
-   public KryonettyServerInitializer(Endpoint endpoint) {
-   	this.endpoint = endpoint;
-   	SERVER_KRYO = endpoint.getKryo();
-   	SERVER_HANDLER = new KryonettyServerHandler(endpoint);
-   	DECODER = new KryonettyDecoder(SERVER_KRYO);
-   	ENCODER = new KryonettyEncoder(SERVER_KRYO, 4 * 1024, 16 * 1024);
-   }
-   
-   @Override
-   public void initChannel(SocketChannel ch) throws Exception {
-       ChannelPipeline pipeline = ch.pipeline();
 
-       // Add the text line codec combination first,
-       pipeline.addLast(new LoggingHandler(LogLevel.INFO));
-       // the encoder and decoder are static as these are sharable
-       pipeline.addLast(DECODER);
-       pipeline.addLast(ENCODER);
+    private final Endpoint endpoint;
+    private final KryonettyServerHandler serverHandler;
 
-       // and then business logic.
-       pipeline.addLast(new KryonettyServerHandler(endpoint));
-   }
+    public KryonettyServerInitializer(Endpoint endpoint) {
+        this.endpoint = endpoint;
+        this.serverHandler = new KryonettyServerHandler(endpoint);
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // Add the text line codec combination first,
+        pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+        pipeline.addLast(new KryonettyDecoder(endpoint));
+        pipeline.addLast(new KryonettyEncoder(endpoint));
+
+        // and then business logic.
+        pipeline.addLast(new KryonettyServerHandler(endpoint));
+    }
 }
